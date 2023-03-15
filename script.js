5dfcd148c5b51b417e1acb31f6cf97247a2688bc
SANTIAGO DOFF <46873259@ort>
2023-03-15 12:14:31 -0300

    Paici

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-var notaMate = document.getElementById('nota1').value;
-console.log(notaMate);
-var notaLengua = document.getElementById('nota2').value;
-var notaEFSI = document.getElementById('nota3').value;
-function CalcularPromedio(nota1, nota2, nota3){
-    var pro = (nota1 + nota2 + nota3)/3;
+let notamate;
+let notalengua;
+let notaefsi
+function enviarForm(evento) {
+    evento.preventDefault();
+    
+    notamate = parseInt(document.getElementById("nota1").value);
+    notalengua = parseInt(document.getElementById("nota2").value);
+    notaefsi = parseInt(document.getElementById("nota3").value);
+}
+document.getElementById("myForm").addEventListener("submit", enviarForm);
+function CalcularPromedio() {
+    var pro = (notamate + notalengua + notaefsi) / 3;
     console.log(pro);
-    return pro;
+}
+function NotaMasAlta(){
+    if (notamate >= notalengua && notamate >= notaefsi) {
+        return console.log("notamate");
+    } 
+    else if (notalengua >= notamate && notalengua >= notaefsi) {
+        return console.log("notalengua");
+    } 
+    else {
+        console.log("notaefsi");
+    }
 }
